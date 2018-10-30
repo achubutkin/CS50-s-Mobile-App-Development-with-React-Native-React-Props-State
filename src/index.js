63f7c55d89ba3a3ba405ac52fb80771fe259016c
achubutkin <a.chubutkin@gmail.com>
2018-10-30 20:32:45 +0600

    Add Toggle Todo feature

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ let id = 0;
 
 const Todo = props => (
   <li>
-    <input type="checkbox" />
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onChange={props.onToggle}
+    />
     <button onClick={props.onDelete}>delete</button>
     <span>{props.todo.text}</span>
   </li>
@@ -29,7 +33,7 @@ class App extends React.Component {
     let text = prompt("Please, write Todo: ");
     if (text) {
       this.setState({
-        todos: [...this.state.todos, { id: id++, text: text }]
+        todos: [...this.state.todos, { id: id++, text: text, checked: false }]
       });
     }
   }
@@ -40,13 +44,30 @@ class App extends React.Component {
     });
   }
 
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      })
+    });
+  }
+
   render() {
     return (
       <div>
         <button onClick={() => this.addTodo()}>Add TODO</button>
         <ul>
           {this.state.todos.map(todo => (
-            <Todo onDelete={() => this.removeTodo(todo.id)} todo={todo} />
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
           ))}
         </ul>
       </div>
