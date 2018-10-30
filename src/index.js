59d0cee2a7a28a43de0f9aaa4b4df309b7d75e74
achubutkin <a.chubutkin@gmail.com>
2018-10-30 19:42:45 +0600

    Creating a todo list

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <ul />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
